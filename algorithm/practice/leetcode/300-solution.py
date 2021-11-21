3be1e77f92e77dff82a791937a4da7fe219f3ce3
yym-mac <492851378@qq.com>
2021-11-21 22:25:43 +0800

    leecode: 300-solution

---
+++ algorithm/practice/leetcode/300-solution.py
@@ -0,0 +1,29 @@
+"""
+300. 最长递增子序列
+https://leetcode-cn.com/problems/longest-increasing-subsequence/
+
+题目描述:
+给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+
+示例 1：
+输入：nums = [10,9,2,5,3,7,101,18]
+输出：4
+解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+"""
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * len(nums)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        
+        return max(dp)
+
+
+result = Solution().lengthOfLIS([10,9,2,5,3,7,101,18])
+print(result)
