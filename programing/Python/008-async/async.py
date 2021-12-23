6023c7530afa10db9ff89468268757cd0500d998
yym-mac <492851378@qq.com>
2021-12-23 10:32:05 +0800

    python: async

--- programing/Python/008-async/async.py
+++ programing/Python/008-async/async.py
@@ -1,22 +1,29 @@
 import asyncio
+import time
+
+
+def time_print(*args, **kwargs) -> str:
+    t = time.perf_counter()
+
+    minute = int(t / 60)
+    second = t % 60
+
+    print(f'{minute:02d}:{second:05.2f}', end=' ')
+    print(*args, **kwargs)
+
 
 async def clean():
-    print("拖地: 开始拖地！！")
+    time_print("拖地: 开始拖地！！")
     await asyncio.sleep(1)
-    print("拖地: 拖地完成！！")
+    time_print("拖地: 拖地完成！！")
 
 async def wash():
-    print("洗衣服: 拿衣服到洗衣机去洗")
-    print("洗衣服: 开始洗衣服...")
+    time_print("洗衣服: 开始洗衣服...")
     await asyncio.sleep(2)
-    print("洗衣服: 洗衣服完毕...")
+    time_print("洗衣服: 洗衣服完毕...")
 
 async def main():
     await asyncio.gather(wash(), clean())
 
 if __name__ == "__main__":
-    import time
-    s = time.perf_counter()
     asyncio.run(main())
-    elapsed = time.perf_counter() - s
-    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
